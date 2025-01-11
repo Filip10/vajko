ecfe12d2ec5d6dbc73b37ca55858780a6dd3b8d2
filip <25711588+Filip10@users.noreply.github.com>
2025-01-11 08:43:26 +0100

    added password/user verification

--- App/Auth/SimpleAuthenticator.php
+++ App/Auth/SimpleAuthenticator.php
@@ -1,15 +1,29 @@
 <?php
 namespace App\Auth;
+use App\Core\AControllerBase;
+use App\Models\User;
+
 class SimpleAuthenticator extends DummyAuthenticator {
 
     public function  login($login, $password): bool
     {
-        // user is logged in when login equals password
-        if ($login == $password) {
+        $users = User::getAll();
+
+        if (isset($users[$login])) { //ked sa meno nachadza v DB
+            if (password_verify($password, $users[$login])) { //skontroluj heslo
+                $_SESSION['user'] = $login;
+                return true;
+            } else {
+                return false;
+            }
+        } else {//ked sa meno nenachadza v DB, vytvor ho
+            $user = new User();
+            $user->setName($login);
+            $user->setPassword(password_hash($password, PASSWORD_DEFAULT));
+            $user->save();
+
             $_SESSION['user'] = $login;
             return true;
-        } else {
-            return false;
         }
     }
 
---
+++ App/Models/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+
+class User extends Model
+{
+    protected int $id;
+    protected string $name;
+    protected string $password;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+}
