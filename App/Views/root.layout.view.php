c59b9d69fcac6396b6f2d53b2d75602b3cff2c0f
filip <25711588+Filip10@users.noreply.github.com>
2025-01-11 11:32:05 +0100

    css now autoupdates

--- App/Views/root.layout.view.php
+++ App/Views/root.layout.view.php
@@ -13,7 +13,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
             crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../public/css/styl.css">
+    <link rel="stylesheet" href="../../public/css/styl.css?v=<?php echo time(); ?>">
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Eighth navbar example">
