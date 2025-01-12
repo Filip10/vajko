0a44c9390b7730fa91349e67ab48870c62ed62a4
filip <25711588+Filip10@users.noreply.github.com>
2025-01-12 12:21:39 +0100

    like fix pri neprihlásených useroch

--- public/js/like.js
+++ public/js/like.js
@@ -3,24 +3,26 @@ window.onload = async () => {
         if (event.target.matches('.likeAJAX')) {
             const button = event.target;
 
-            const idPostu = button.getAttribute('data-id');
-            const data = {id: idPostu, pocetLikov: 0};
+            if (button.getAttribute('data-userLogged') !== '') {
+                const idPostu = button.getAttribute('data-id');
+                const data = {id: idPostu, pocetLikov: 0};
 
-            // Send the fetch request for the clicked button only
-            fetch('http://localhost/?c=Like&a=toggleViaJson', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-                .then(response => response.json())
-                .then(data => {
-                    button.innerHTML = `${data.pocetLikov} ľudia to označili ako užitočné`;
+                // Send the fetch request for the clicked button only
+                fetch('http://localhost/?c=Like&a=toggleViaJson', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
                 })
-                .catch((error) => {
-                    button.innerText = 'Error: ' + error;
-                });
+                    .then(response => response.json())
+                    .then(data => {
+                        button.innerHTML = `${data.pocetLikov} ľudia to označili ako užitočné`;
+                    })
+                    .catch((error) => {
+                        button.innerText = 'Error: ' + error;
+                    });
+            }
         }
     });
 }
