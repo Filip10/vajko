7b6e94c2e3bf80cd84e30dd4bdaad3e62c60c912
filip <25711588+Filip10@users.noreply.github.com>
2025-01-12 14:51:37 +0100

    css rules added

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -3,4 +3,4 @@
 <h2 class="obsah">O projekte</h2>
 <p class="obsah">Cieľom tohto projektu je priniesť nadšencom dopravy aktuálne informácie o dopravných stavbách na Slovensku.</p>
 <p class="obsah">Informácie sú dopĺňané komunitou nadšencov, ktorých sa zaujímajú o aktuálne diane v doprave na Slovensku.</p>
-<p class="obsah">Na stránke ešte pracujeme. Aktuálne pripravené stránky sú diaľnice, rýchlostné cesty, ostatné a login.</p>
+<p class="obsah">Na stránke ešte pracujeme.</p>
